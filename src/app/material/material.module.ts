2a3b7578100b9afc7922c6692161c808c5492d91
jonahDs <jonah.dev@hotmail.com>
2020-08-28 19:59:21 +0200

    added mock server

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatRippleModule } from '@angular/material/core';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatGridListModule, MatRippleModule],
+  exports: [MatGridListModule, MatRippleModule, MatListModule],
 })
 export class MaterialModule {}
